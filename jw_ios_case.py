3e88d977f35f7cf6e77c65676cf6ccb3c6255311
Karin <Karinzl@163.com>
2022-09-14 17:40:51 +0800

    123
    Signed-off-by: Karin <Karinzl@163.com>

--- jw_ios_case.py
+++ jw_ios_case.py
@@ -1,5 +1,7 @@
 #-*- coding:utf-8 -*-
 from time import sleep
+
+import ele as ele
 import wda
 import os
 import re
@@ -23,21 +25,29 @@ class JiWei:
         #with c.alert.watch_and_click(['提示', '取消']):
              #c(name="取消").click()
         c.session('com.co.Yoosee')#打开app
-        c(type='XCUIElementTypeSecureTextField').set_text("qwe12345")
+        c(type='XCUIElementTypeTextField').clear_text() #清除当前账号
+        c(type='XCUIElementTypeTextField').set_text("13016129715") #输入手机号
+        c(type='XCUIElementTypeSecureTextField').set_text("qwe12345")# 输入正确密码
         c(name="登录").click()
         c(name="同意并继续").click()
         c(name="以后").click()
-        c(name="我的").click()
-        c(type="XCUIElementTypeStaticText").click()
-        c(accessibility id="0台设备")
-
+        if c(type='XCUIElementTypeStaticText'):#如果有添加设备按钮则退出登录
+            c(name="我的").click()
+            c(type="XCUIElementTypeStaticText").click()
+            c(name="退出登录").click()
+        return False
         sleep(5)
         c.session().app_terminate('com.co.Yoosee')  # 关闭app
 
     @classmethod
     def jwt_14(cls, c, video_camera_name):#周云婷手机登录
         c.session('com.co.Yoosee')
-        c()
+        c(name="微信").click()
+        c(name="同意并继续").click()
+        c(name="更多选项").click()
+        c(name="登录其他帐号").click()
+        sleep(5)
+        c.session().app_terminate('com.co.Yoosee')  # 关闭app
 
 
 
